270e0e57e5b21366c74065d489c439d86ee9db88
leosid-dev <140845661+leosid-dev@users.noreply.github.com>
2025-03-15 17:00:46 +0530

    updates

--- pycachesim/pintool/pinMarker.h
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-
-static volatile bool _pinMarker_active = false;
-
-void  __attribute__ ((noinline)) _magic_pin_start(){_pinMarker_active=true;};
-void  __attribute__ ((noinline)) _magic_pin_stop(){_pinMarker_active=false;};
